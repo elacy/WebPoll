698dfd6df5f71b74b085e8dfd8c9c7ef7355a6f1
evalacy <eva@lacy.ie>
2015-01-30 16:30:25 +0000

    Added basic poll form

---
+++ src/WebPoll.Web/Controllers/PollController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPoll.Web.Controllers
+{
+    public class PollController : Controller
+    {
+        //
+        // GET: /Poll/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
